8148e14b2c7f29c6112d050d3456eba63a479a35
enextus <enextus@gmail.com>
2021-07-03 14:27:51 +0200

    21_07_03_

--- src/test/java/com/appium/sanity/AppiumHelloWorld.java
+++ src/test/java/com/appium/sanity/AppiumHelloWorld.java
@@ -1,28 +1,39 @@
 package com.appium.sanity;
 
+import com.google.gson.internal.bind.util.ISO8601Utils;
 import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
+import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 
 public class AppiumHelloWorld {
 
-    private AndroidDriver driver;
-    private String APP = "https://github.com/cloudgrey-io/the-app/releases/download/v1.7.1/TheApp-v1.7.1.apk";
 
+    File rootPath = new File(System.getProperty("user.dir"));
+    File src = new File(rootPath,"/Downloads/");
+    File appPath = new File(src,"TheApp-v1.7.1.apk");
+
+
+
+    private AndroidDriver driver;
+    // private String APP = "https://github.com/cloudgrey-io/the-app/releases/download/v1.7.1/TheApp-v1.7.1.apk";
     @Before
     public void setUp() throws MalformedURLException {
         DesiredCapabilities capabilities = new DesiredCapabilities();
 
         capabilities.setCapability("platformName", "Android");
         capabilities.setCapability("deviceName", "Nexus");
-        capabilities.setCapability("avd", "Nexus");
+        capabilities.setCapability("avd", "qa14_mob_nexus5");
         capabilities.setCapability("automationName", "UiAutomator2");
-        capabilities.setCapability("app", APP);
+        // capabilities.setCapability("app", APP);
+        capabilities.setCapability("app", appPath.getAbsolutePath());
+
 
         driver = new AndroidDriver(new URL("http://localhost:4723/wd/hub"), capabilities);
     }
@@ -36,7 +47,7 @@ public class AppiumHelloWorld {
 
     @Test
     public void appiumHelloWorld() throws InterruptedException {
-        Thread.sleep(50000);
+        Thread.sleep(20000);
         driver.findElementByAccessibilityId("Login Screen").click();
     }
 }
